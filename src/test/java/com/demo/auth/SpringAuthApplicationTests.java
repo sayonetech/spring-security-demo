a025ce4118ae72aa7ffb30cc22410b0b6433252b
initiuminc1 <initiuminc1@gmail.com>
2020-04-25 17:40:19 +0530

    first commit

---
+++ src/test/java/com/demo/auth/SpringAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

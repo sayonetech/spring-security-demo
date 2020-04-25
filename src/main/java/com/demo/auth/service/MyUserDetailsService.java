8cf498cd323a8dec14844f67302e0b33372d2bf7
initiuminc1 <initiuminc1@gmail.com>
2020-04-25 18:05:27 +0530

    first commit

--- src/main/java/com/demo/auth/service/MyUserDetailsService.java
+++ src/main/java/com/demo/auth/service/MyUserDetailsService.java
@@ -4,7 +4,9 @@ import com.demo.auth.model.MyUserDetails;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
     @Override

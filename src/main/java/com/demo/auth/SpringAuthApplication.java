05c11c2b56be05dc3d28f1435802cb953b276aaa
initiuminc1 <initiuminc1@gmail.com>
2020-04-25 17:53:29 +0530

    first commit

--- src/main/java/com/demo/auth/SpringAuthApplication.java
+++ src/main/java/com/demo/auth/SpringAuthApplication.java
@@ -2,12 +2,18 @@ package com.demo.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringAuthApplication {
+public class SpringAuthApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAuthApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringAuthApplication.class);
+	}
 }
---
+++ src/main/java/com/demo/auth/service/MyUserDetailsService.java
@@ -0,0 +1,14 @@
+package com.demo.auth.service;
+
+import com.demo.auth.model.MyUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new MyUserDetails(username);
+    }
+}

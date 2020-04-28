ed5bcdb7d578e626145695e643df3c190caeba0c
initiuminc1 <initiuminc1@gmail.com>
2020-04-28 14:07:51 +0530

    user entity

--- src/main/java/com/demo/auth/model/User.java
+++ src/main/java/com/demo/auth/model/User.java
@@ -7,9 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
